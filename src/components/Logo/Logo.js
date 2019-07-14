47cbc8d4ec063699b23f7d9b15a3bcb3953eb842
tien95vt <ttienn123@gmail.com>
2019-07-14 23:04:52 +0700

    add logo react

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import burgerLogo from '../../assets/images/burger-logo.png';
+import reactLogoImg from '../../assets/images/react_logo.png';
 import classes from './Logo.module.css';
 
 const logo = (props) => (
     <div className={classes.Logo} style={{height: props.height}}>
         <img src={burgerLogo} alt="My logo" />
+        <img src={reactLogoImg} alt="React logo" />
     </div>
 );
 
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from '../../../axios-orders';
-import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Button from '../../../components/UI/Button/Button';
